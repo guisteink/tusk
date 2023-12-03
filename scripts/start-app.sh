72fc343a9ae3446af204f2bc54439a46380d7e33
Guilherme <guilherme.stein@zenvia.com>
2023-12-03 14:55:31 -0300

    feat: post-modules

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,1 +1,1 @@
-DATABASE_URI="mongodb+srv://admin:admin@4uth.9fbuvlh.mongodb.net/?retryWrites=true&w=majority" go run ../cmd/app/main.go
+DATABASE_URI="mongodb+srv://admin:qwe123@4uth.9fbuvlh.mongodb.net/tusk" go run ../cmd/app/main.go
